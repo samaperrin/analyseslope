fc451f2ddf46c3ae2e98b54fad48a8cf678c5a2e
samaperrin <sam.perrin@ntnu.no>
2018-02-09 17:47:37 +0100

    Built vignette, added to analyse slope description

---
+++ R/analyseslope.R
@@ -0,0 +1,11 @@
+#' analyseslope: A package which performs Bayesian analysis and visualization for slope parameters.
+#'
+#' The package contains functions which extracts slope parameters from our database,
+#' plot these slope parameters, and analyse them. We can also identify outliers.
+#'
+#' @section analyseslope functions:
+#' The analyseslope functions ...
+#'
+#' @docType package
+#' @name analyseslope
+NULL
