da8c6f36c431f371198c538118b517e230daaf58
samaperrin <sam.perrin@ntnu.no>
2018-03-20 10:40:27 +0100

    Added species name to plots

--- R/plot_beta.R
+++ R/plot_beta.R
@@ -9,12 +9,12 @@
 #'
 #' @export
 
-plot_beta <- function(slope_analysis_fish,parameters_to_plot) {
+plot_beta <- function(slope_analysis_fish,parameters_to_plot,species) {
   fsum <- slope_analysis_fish$summary[parameters_to_plot,]
   min.y <- min(fsum$`2.5%`)
   max.y <- max(c(fsum$`97.5%`),0.5)
   plot(0, 0, xlim = c(1, length(parameters_to_plot)), ylim = c(min.y, max.y), type = "n",xaxt='n',xlab='',
-       ylab = "Beta distribution", main="")
+       ylab = paste0("Beta distribution (",species,")"), main="")
   abline(h = 0,col = "grey")
   arrows(x0 = 1:length(parameters_to_plot), x1 = 1:length(parameters_to_plot), y0 = fsum[, "2.5%"], y1 = fsum[, "97.5%"], code = 3, angle = 90, length = 0.05,lwd=2)
   axis(side=1,at = 1:length(parameters_to_plot),labels=parameters_to_plot,las=2,cex.axis=0.7)
