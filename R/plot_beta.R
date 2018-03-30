abebb615f14cb27ce4da4a47cb6f22eaf10f66a7
samaperrin <sam.perrin@ntnu.no>
2018-03-30 08:36:01 +0200

    Shows significance of parameters

--- R/plot_beta.R
+++ R/plot_beta.R
@@ -14,8 +14,13 @@ plot_beta <- function(slope_analysis_fish,parameters_to_plot,species) {
   min.y <- min(fsum$`2.5%`)
   max.y <- max(c(fsum$`97.5%`),0.5)
   plot(0, 0, xlim = c(1, length(parameters_to_plot)), ylim = c(min.y, max.y), type = "n",xaxt='n',xlab='',
-       ylab = paste0("Beta distribution (",species,")"), main="")
+       ylab = paste0("Beta distribution (",species,")"), main="Blue lines indicate parameter significance")
   abline(h = 0,col = "grey")
-  arrows(x0 = 1:length(parameters_to_plot), x1 = 1:length(parameters_to_plot), y0 = fsum[, "2.5%"], y1 = fsum[, "97.5%"], code = 3, angle = 90, length = 0.05,lwd=2)
+  arrow_col <- character()
+  for (i in 1:7) {
+    arrow_col[i] <- ifelse(fsum[i,"97.5%"]<0,"blue","black")
+  }
+  arrows(x0 = 1:length(parameters_to_plot), x1 = 1:length(parameters_to_plot), y0 = fsum[, "2.5%"], y1 = fsum[, "97.5%"], code = 3, angle = 90, length = 0.05,lwd=2,col=arrow_col)
+  points(x = 1:length(parameters_to_plot), y = fsum[, "50%"],col=arrow_col)
   axis(side=1,at = 1:length(parameters_to_plot),labels=parameters_to_plot,las=2,cex.axis=0.7)
 }
